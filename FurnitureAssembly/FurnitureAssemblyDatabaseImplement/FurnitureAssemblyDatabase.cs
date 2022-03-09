1de74ef6540b500ae7ba3689b8d933b6a6450850
taurielke <taurielkee@yandex.ru>
2022-03-09 12:43:45 +0400

    committing lab3

---
+++ FurnitureAssembly/FurnitureAssemblyDatabaseImplement/FurnitureAssemblyDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FurnitureAssemblyDatabaseImplement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureAssemblyDatabaseImplement
+{
+    public class FurnitureAssemblyDatabase : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured == false)
+            {
+                optionsBuilder.UseSqlServer(@"Data Source=LAPTOP-3ONQPAF5\SQLEXPRESS;Initial Catalog=FurnitureAssemblyDatabase;Integrated Security=True;MultipleActiveResultSets=True;");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public virtual DbSet<Component> Components { set; get; }
+        public virtual DbSet<Furniture> Furnitures { set; get; }
+        public virtual DbSet<FurnitureComponent> FurnitureComponents { set; get; }
+        public virtual DbSet<Order> Orders { set; get; }
+    }
+}
